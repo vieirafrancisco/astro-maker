b92000514c157f801e0992248a8aa920f491a388
Francisco Neto <fvcneto.master98@gmail.com>
2021-01-31 21:58:11 -0300

    feat: create basic force logic

--- astro/src/sprites/player.py
+++ astro/src/sprites/player.py
@@ -1,5 +1,7 @@
 import pygame as pg
 
+vector = pg.Vector2
+
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
         groups = [game.all_sprites]
@@ -8,7 +10,28 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((64, 64))
         self.image.fill((255,255,255))
         self.rect = self.image.get_rect()
-        self.rect.topleft = (100, 100)
+        self.pos = vector(100, 100)
+        self.rect.topleft = self.pos
+        self.acc = vector(0, 0)
+        self.vel = vector(0, 0)
 
     def update(self):
-        pass
+        self.move()
+        if self.rect.bottomright[1] < 768:
+            self.acc = vector(0, 1) * 0.2 # gravity
+        else:
+            self.acc *= 0
+            self.vel *= 0
+            self.pos = (self.pos.x, 704)
+        self.vel += self.acc
+        self.pos += self.vel
+        self.rect.topleft = self.pos
+
+    def move(self):
+        dx, dy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            dx -= 5
+        if keys[pg.K_RIGHT]:
+            dx += 5
+        self.pos += (dx, dy)
